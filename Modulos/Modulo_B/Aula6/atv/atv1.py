ee6fd7ff7b305173107fae84c9275bccfede2c41
guilherme vieira <guizinho.vfs@gmail.com>
2022-05-17 23:25:05 -0300

    Aula 6 - Tipos primitvos

---
+++ Modulos/Modulo_B/Aula6/atv/atv1.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o 1° valor: "))
+# Variável numero 1
+n2 = int(input("Digite o 2° valor: "))
+s = n1 + n2
+# Variável numero 2
+print("A soma entre {} e {} vale{}".format(n1, n2, s))
+# saída de dados
---
+++ Modulos/Modulo_B/Aula6/atv/atv2.py
@@ -0,0 +1,4 @@
+v = bool(input("Digite algum valor: "))
+# Variável valor
+print(v)
+# retornando valor: True ou False
